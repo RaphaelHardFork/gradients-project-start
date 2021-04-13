8a19506488552503c8d257750c49fe1fb5b29828
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-04-13 11:27:12 +0200

    coquille

--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -3,16 +3,19 @@ import Gradient from "./Gradient"
 const GradientsList = (props) => {
   const { gradients, filter, setFilter } = props
 
+  const filteredList = gradients.filter((elem) => {
+    if (filter === 'Tous') {
+      return true
+    } else {
+      return elem.tags.includes(filter)
+    }
+  })
+
   return (
     <ul className="row list-unstyled">
-      {gradients.map((elem) => {
-        if (filter === 'Tous') {
-          return <li key={elem.name} className="col-lg-3 col-md-4 col-sm-6"><Gradient setFilter={setFilter} filter={filter} colorStart={elem.start} colorEnd={elem.end} name={elem.name} tags={elem.tags} /></li>
-        } else if (elem.tags.includes(filter)) {
-          return <li key={elem.name} className="col-lg-3 col-md-4 col-sm-6"><Gradient setFilter={setFilter} filter={filter} colorStart={elem.start} colorEnd={elem.end} name={elem.name} tags={elem.tags} /></li>
-        }
-      }
-      )}
+      {filteredList.map((elem) => {
+        return <li key={elem.name} className="col-lg-3 col-md-4 col-sm-6"><Gradient setFilter={setFilter} filter={filter} colorStart={elem.start} colorEnd={elem.end} name={elem.name} tags={elem.tags} /></li>
+      })}
     </ul>
   )
 }
